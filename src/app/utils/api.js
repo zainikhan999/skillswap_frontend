1407d6d437d87f6ae5b4c97cf0b8d73570335368
zainikhan999 <zk.032918@gmail.com>
2025-10-02 17:05:36 +0500

    changed the crsf token link

--- src/app/utils/api.js
+++ src/app/utils/api.js
@@ -1,32 +1,3 @@
-// // src/api.js
-// import axios from "axios";
-// const BASE_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
-// const api = axios.create({
-//   baseURL: BASE_URL, // change this to your backend URL in prod
-//   withCredentials: true, // send cookies automatically
-// });
-
-// let csrfToken = null;
-
-// // Interceptor for CSRF protection
-// api.interceptors.request.use(async (config) => {
-//   const method = config.method?.toLowerCase();
-
-//   if (["post", "put", "delete", "patch"].includes(method)) {
-//     if (!csrfToken) {
-//       const res = await axios.get(`${BASE_URL}/api/csrf-token`, {
-//         withCredentials: true,
-//       });
-//       csrfToken = res.data.csrfToken;
-//     }
-//     config.headers["X-CSRF-Token"] = csrfToken;
-//   }
-
-//   return config;
-// });
-
-// export default api;
-// src/utils/api.js
 import axios from "axios";
 
 const BASE_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
@@ -42,9 +13,10 @@ let csrfToken = null;
 // Function to fetch fresh CSRF token
 const fetchCSRFToken = async () => {
   try {
-    const response = await api.get(`${BASE_URL}/api/csrf-token`, {
+    const response = await api.get("/api/csrf-token", {
       withCredentials: true,
     });
+
     csrfToken = response.data.csrfToken;
     console.log("CSRF token fetched:", csrfToken ? "SUCCESS" : "FAILED");
     return csrfToken;
