cc5f63df9ff0882d858125957a7b4898be71b6c8
zainikhan999 <zk.032918@gmail.com>
2025-10-28 17:37:29 +0500

    tried solving browsing issues (private) + iphone users

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source:
+          "/:path((?!_next/|favicon\\.ico|images/|assets/|socket\\.io/).*)",
+        destination: "https://api.skillswapbackend.kesug.com/:path*",
+      },
+    ];
+  },
+};
 
 export default nextConfig;
